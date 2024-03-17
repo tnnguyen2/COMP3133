a51304b0c07ac1becf786eae690523c3c1b31e99
TDotN <TDotN@DESKTOP-LAYZEEB>
2024-03-17 16:28:12 -0400

    Week10_Lab6_Exercise

---
+++ Week10_FRI/Lab6_100907949_COMP3133/exercise-1/myapp/routes/users.js
@@ -0,0 +1,20 @@
+var express = require('express');
+var bodyParser = require('body-parser');
+
+var router = express.Router();
+
+/* GET users listing. */
+router.get('/', function(req, res, next) {
+  res.send('respond with a resource');
+});
+
+// POST method route
+router.post('/', function (req, res) {
+  console.log(`First Name: ${req.body.firstName}`);
+  console.log(`Last Name: ${req.body.lastName}`);
+  res.send('POST received!');
+});
+
+router.use(bodyParser.urlencoded({ extended: true }));
+
+module.exports = router;
