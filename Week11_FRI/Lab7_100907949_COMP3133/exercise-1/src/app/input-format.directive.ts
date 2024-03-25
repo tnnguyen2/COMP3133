c7da54fffd015c6e86e92a1e0ba8f4400cc2620d
TDotN <TDotN@DESKTOP-LAYZEEB>
2024-03-25 00:36:06 -0400

    Week11_Lab7_Exercise

---
+++ Week11_FRI/Lab7_100907949_COMP3133/exercise-1/src/app/input-format.directive.ts
@@ -0,0 +1,15 @@
+import { Directive, HostListener, ElementRef } from '@angular/core';
+
+@Directive({
+  selector: '[appInputFormat]',
+  standalone: true,
+})
+export class InputFormatDirective {
+
+  constructor(private el: ElementRef) { }
+  @HostListener('blur') onBlur() {
+    let value: string = this.el.nativeElement.value;
+    this.el.nativeElement.value = value.toUpperCase();
+  }
+
+}
